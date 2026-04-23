9d51b7742d442fdb260fd51bad212a5fb8943321
HoangViet <hoangviet1121999@gmail.com>
2026-04-23 14:13:26 +0700

    change: port deployed

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,7 +53,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:9121"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
